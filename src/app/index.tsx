a697eac621ef6fdb28c0484adadf92a3ffd010b5
Bholanath <bholanathbarik9748@gmail.com>
2024-11-04 20:11:49 +0530

    Feat: create new project and add expo-route

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import HomePage from "../screen/HomePage/HomePage";
+
+const index = () => {
+  return <HomePage />;
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
